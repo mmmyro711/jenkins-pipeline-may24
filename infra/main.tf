3d1c1fe26ad716166ef5daa3c9b0d9cb2bb993d9
Myroslav Yaremenko <ymiroshavs@gmail.com>
2024-11-05 20:32:55 -0600

    1

--- infra/main.tf
+++ infra/main.tf
@@ -1,27 +1,8 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "myjenkins_server" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.instance_type
-
-  key_name               = "new-key-pair-east1"
-
+resource "aws_instance" "web" {
+  ami           = "ami-0bb84b8ffd87024d8"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "jenkins-class-work"
+    Name = "terraform-session-instance"
   }
 }
