764eebccbc49b9e2b10047dd6cea16d2c2bd0364
Myroslav Yaremenko <ymiroshavs@gmail.com>
2024-11-07 18:35:41 -0600

    test need file

--- directives.groovy
+++ directives.groovy
@@ -2,12 +2,17 @@ pipeline {
     agent any 
     environment {
         Var1 = "building"
+        Var2 = "testing"
+        Var3 = "success"
     }
     stages {
         stage('build') {
             steps {
                 echo " This is ${Var1}"
             }
+        }
+        stage('Test'){
+            echo "This is ${env.Var2}"
         }
     }
 }
